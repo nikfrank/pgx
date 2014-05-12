6478eddb1f4e1464e7b773981600a658ed11e8f8
nik frank <nikbfrank@gmail.com>
2014-05-12 23:28:18 +0300

    delims done

--- pgwrap.js
+++ pgwrap.js
@@ -29,10 +29,11 @@ module.exports = function(pg, conop, schemas){
     
     pg.insert = function(schemaName, query, options, callback){
 	
-	var qreq = 'insert into api_'+schemaName+' (';
-	var valreq = ') values (';
-
+// use schema.tableName
 	var schema = schemas.db[schemaName];
+
+	var qreq = 'insert into '+schema.tableName+' (';
+	var valreq = ') values (';
 
 	if(!('hash' in query)){
 	    // make up a hash
@@ -40,12 +41,12 @@ module.exports = function(pg, conop, schemas){
             var hash2 = hmac.update(''+((new Date()).getTime())+''+Math.random());
             var digest = hmac.digest(encoding="base64");
 
-	    query.hash = digest;//28chars
+	    query.hash = digest;//28chars good enough
 	}
 
 //extend this for multidim arrays?
 	for(var ff in schema.fields){
-	    if(ff === 'xattrs') continue;
+	    if(ff === schemaName+'_xattrs') continue;
 	    if(!(ff in query)) if('defval' in schema.fields[ff]) query[ff] = schema.fields[ff].defval;
 
 	    if(ff in query){
@@ -132,14 +133,14 @@ module.exports = function(pg, conop, schemas){
 	//put the other fields of query into xattrs if any
 	var isx = false;
 	var xat = {};
-	if('xattrs' in query){
-	    xat = query.xattrs;
+	if(schemaName+'_xattrs' in query){
+	    xat = query[schemaName+'_xattrs'];
 
 	    isx = (JSON.stringify(xat).length>2)
 	}
 
 	for(var ff in query){
-	    if(ff === 'xattrs') continue;
+	    if(ff === schemaName+'_xattrs') continue;
 
 	    if(!(ff in schema.fields)){
 		// put into xattrs
@@ -148,7 +149,7 @@ module.exports = function(pg, conop, schemas){
 	    }
 	}
 	if(isx){
-	    qreq += 'xattrs,';
+	    qreq += schemaName+'_xattrs,';
 
 	    var dm = dmfig(xat);
 	    valreq += dm + JSON.stringify(xat) + dm +'::json,';// json of xat
@@ -179,13 +180,14 @@ module.exports = function(pg, conop, schemas){
 
     pg.update = function(schemaName, input, options, callback){
 	
-	var qreq = 'update api_'+schemaName+' set ';
+// use schema.tableName
+	var schema = schemas.db[schemaName];
+
+	var qreq = 'update '+schema.tableName+' set ';
 	var wreq = ' where ';
 
 	var where = input.where;
 	var query = input.data;
-
-	var schema = schemas.db[schemaName];
 	
 //slap together wreq out of the where collection
 // this obv only works for string and number queries right now
@@ -204,7 +206,8 @@ module.exports = function(pg, conop, schemas){
 	wreq = wreq.substr(0, wreq.length-4);
 
 	// maybe only select what's being updated
-	var sreq = 'select * from api_'+schemaName+wreq+';';
+// use schema.tableName
+	var sreq = 'select * from '+schema.tableName+wreq+';';
 
 // select the record, which for now should be unique
 
@@ -234,7 +237,7 @@ module.exports = function(pg, conop, schemas){
 
 		//extend this for multidim arrays?
 		for(ff in query){
-		    if(ff === 'xattrs') continue;
+		    if(ff === schemaName+'_xattrs') continue;
 		    if(!(ff in schema.fields)) continue;
 		    qreq += ff + '=';
 		    var dm = dmfig(query[ff]);
@@ -308,9 +311,9 @@ module.exports = function(pg, conop, schemas){
 
 		//put the other fields of query into xattrs if any
 		var isx = false;
-		var xat = doc.xattrs;
+		var xat = doc[schemaName+'_xattrs'];
 		for(var ff in query){
-		    if(ff === 'xattrs') continue;
+		    if(ff === schemaName+'_xattrs') continue;
 		    if(!(ff in schema.fields)){
 			// put into xattrs
 			isx = true;
@@ -319,7 +322,7 @@ module.exports = function(pg, conop, schemas){
 		    }
 		}
 		if(isx){
-		    qreq += 'xattrs=';
+		    qreq += schemaName+'_xattrs=';
 		    //determine delimiter
 		    var dm = dmfig(xat);
 		    qreq += dm + JSON.stringify(xat) + dm +'::json,';// json of xat
@@ -347,7 +350,8 @@ module.exports = function(pg, conop, schemas){
 	var schema = schemas.db[schemaName];
 
 // only read fields in options.fields?
-	var qreq = 'select * from api_'+schemaName;
+// use schema.tableName
+	var qreq = 'select * from '+schema.tableName;
 	var wreq = ' where ';
 
 	for(var ff in query){
@@ -382,11 +386,11 @@ module.exports = function(pg, conop, schemas){
 	
 		//loop through result.rows[i].xattrs[ff] -> result.rows[i][ff]
 		for(var i=result.rows.length; i-->0;){
-		    if(!('xattrs' in result.rows[i])) continue;
-		    for(var ff in result.rows[i].xattrs){
-			result.rows[i][ff] = result.rows[i].xattrs[ff];
+		    if(!(schemaName+'_xattrs' in result.rows[i])) continue;
+		    for(var ff in result.rows[i][schemaName+'_xattrs']){
+			result.rows[i][ff] = result.rows[i][schemaName+'_xattrs'][ff];
 		    }
-		    delete result.rows[i].xattrs;
+		    delete result.rows[i][schemaName+'_xattrs'];
 		}
 		done();
 		return callback(err, (result||{rows:[]}).rows);
@@ -398,6 +402,7 @@ module.exports = function(pg, conop, schemas){
 
 
     pg.boot = function(req,res){
+
 	pg.connect(conop, function(err, client, done) {
 	    if(err) return res.json({err:err});
 
@@ -425,7 +430,7 @@ module.exports = function(pg, conop, schemas){
 				qq += ff +' '+ sc.fields[ff].type+',';
 			    }
 			    for(var ff in schemas.defaultFields){
-				qq += ff +' '+ schemas.defaultFields[ff].type+',';
+				qq += sn+'_'+ff +' '+ schemas.defaultFields[ff].type+',';
 			    }
 			    qq = qq.substr(0,qq.length-1) + ');';
 
